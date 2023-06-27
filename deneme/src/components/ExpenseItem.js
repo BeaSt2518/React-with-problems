8df2ac3f2d384bc9bf73dbe641a791669c86fc83
Bahadır Erdem <24529148+BeaSt2518@users.noreply.github.com>
2023-06-28 01:03:38 +0300

    Learned How to divide to smaller components

--- deneme/src/components/ExpenseItem.js
+++ deneme/src/components/ExpenseItem.js
@@ -1,16 +1,10 @@
 import './ExpenseItem.css'
-
+import ExpenseDate from './ExpenseDate';
 function ExpenseItem(props) {
-    const month = props.date.toLocaleString('tr-TR' , {month: 'long'});
-    const day = props.date.toLocaleString('tr-TR' , {day: '2-digit'}); 
-    const year = props.date.getFullYear();
+    
   return (
     <div className="expense-item">
-      <div>
-        <div>{day}</div>
-        <div>{month}</div>
-        <div>{year}</div>
-      </div>
+        <ExpenseDate date={props.date} />
       <div>
         <h2 className="expense-item__description" >{props.title}</h2>
         <div className="expense-item__price">{props.amount} TL</div>
