396337b4469c9766a25cdc9876b73430f6cab860
Bahadır Erdem <24529148+BeaSt2518@users.noreply.github.com>
2023-06-28 00:34:24 +0300

    Update ExpenseItem.js

--- deneme/src/components/ExpenseItem.js
+++ deneme/src/components/ExpenseItem.js
@@ -1,9 +1,16 @@
 import './ExpenseItem.css'
 
 function ExpenseItem(props) {
+    const month = props.date.toLocaleString('tr-TR' , {month: 'long'});
+    const day = props.date.toLocaleString('tr-TR' , {day: '2-digit'}); 
+    const year = props.date.getFullYear();
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <div>
+        <div>{day}</div>
+        <div>{month}</div>
+        <div>{year}</div>
+      </div>
       <div>
         <h2 className="expense-item__description" >{props.title}</h2>
         <div className="expense-item__price">{props.amount} TL</div>
