538a93215920893691d11858bf34a6ee772de839
Bahadır Erdem <24529148+BeaSt2518@users.noreply.github.com>
2023-07-02 18:23:45 +0300

    Added a basic navigation bar

---
+++ deneme/src/components/Navbar.js
@@ -0,0 +1,18 @@
+export default function Navbar() {
+  return (
+    <nav className="nav">
+      <a href="/" className="site-title">
+        Bahadır Erdem
+      </a>
+      <ul className="nav-comp-list">
+        <li>
+          <a href="/pricing">Projelerim</a>
+        </li>
+        <li>
+          <a href="/about">Hakkımda</a>
+        </li>
+      </ul>
+    </nav>
+    
+  );
+}
